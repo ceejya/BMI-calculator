cc12b7882adb1f46d7d3f290c56ee33bc93bd362
ceejya <ceejbeat@gmail.com>
2026-06-22 10:52:10 +0100

    update bmi cal

--- index.js
+++ index.js
@@ -6,33 +6,33 @@ function bmiCheck() {
     let inputValue = inputElement01.value.trim()
     let inputValue1 = inputElement02.value.trim()
 
-    let calculatedBmi = bmi(+inputValue, +inputValue1)
+    if (!inputValue || !inputValue1) {
+        alert('Please fill out with correct inputvalue!');
 
-    if (inputValue == '' || inputValue1 == '') {
-        alert('Please fill out input!');
-
-    }
-    else if (calculatedBmi >= 30) {
-        score.innerHTML = `your BMI is ${Math.ceil(calculatedBmi)}m2, which falls within the obese range`
-
-
-    } else if (calculatedBmi >= 25 && calculatedBmi <= 29.9) {
-        score.innerHTML = `your BMI is ${Math.ceil(calculatedBmi)}m2, which falls within the overweight range`
-
-    } else if (calculatedBmi >= 18.5 && calculatedBmi <= 24.9) {
-        score.innerHTML = `your BMI is ${Math.ceil(calculatedBmi)}m2, which falls within the Normalweight range`
-
+    } else if (inputValue <= 0 || inputValue1 <= 0) {
+        alert('Input value should not be Negative!')
     }
     else {
-        score.innerHTML = `your BMI is ${Math.ceil(calculatedBmi)}m2, which falls within the underweight range`
+        let calculatedBmi = bmi(+inputValue, +inputValue1)
+        if (calculatedBmi >= 30) {
+            score.innerHTML = `your BMI is ${Math.ceil(calculatedBmi)}m2, which falls within the obese range`
+        } else if (calculatedBmi >= 25 && calculatedBmi <= 29.9) {
+            score.innerHTML = `your BMI is ${Math.ceil(calculatedBmi)}m2, which falls within the overweight range`
 
+        } else if (calculatedBmi >= 18.5 && calculatedBmi <= 24.9) {
+            score.innerHTML = `your BMI is ${Math.ceil(calculatedBmi)}m2, which falls within the Normalweight range`
+        }
+        else {
+            score.innerHTML = `your BMI is ${Math.ceil(calculatedBmi)}m2, which falls within the underweight range`
+
+        }
     }
     inputElement01.value = ''
     inputElement02.value = ''
 
 }
 
-function bmi(inputValue, inputValue1) {
-    return inputValue / (inputValue1 * inputValue1)
+function bmi(weight, height) {
+    return weight / (height * height)
 
 }
